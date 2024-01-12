a849645b142eef11622e8443502aad5d60f71209
Sasha <o.yezhurov@gmail.com>
2024-01-12 14:58:48 -0800

    refactored the classes

--- app.py
+++ app.py
@@ -1,37 +1,24 @@
 
 class Bicycle:
     
-    def __init__(self):
-        self.front_wheel = None
-        self.rear_wheel = None
-        self.gear = None
-    
-    def setup_wheels(self, front_wheel=None, rear_wheel=None):
+    def __init__(self, front_wheel, rear_wheel, gear):
         
-        if front_wheel is not None:
-            self.front_wheel = front_wheel
-        
-        if rear_wheel is not None:
-            self.rear_wheel = rear_wheel
-            
-    def setup_gear(self, chainring: int, cog: int):
-        self.gear = Gear(chainring, cog)
+        self.front_wheel = front_wheel
+        self.rear_wheel = rear_wheel
+        self.gear = gear
 
     def gear_inches(self):
         
-        try:
-            result = round(self.rear_wheel.wheel_diameter * self.gear.ratio(), 2)
-        except TypeError:
-            result = 0
-        except AttributeError:
-            result = 0
-        
-        return result
+        return round(
+            self.rear_wheel.wheel_diameter * self.gear.ratio(),
+            2
+        )
 
 
 class Wheel:
 
     def __init__(self, rim_diameter: float, tire_height: float):
+        
         self.rim_diameter = rim_diameter
         self.tire_hight = tire_height
         self.wheel_diameter = rim_diameter + (tire_height * 2)
@@ -40,6 +27,7 @@ class Wheel:
 class Gear:
     
     def __init__(self, chainring: int, cog: int):
+        
         self.chainring = chainring
         self.cog = cog
 
@@ -53,10 +41,12 @@ class Gear:
             return ratio
     
 
-bike1 = Bicycle()
-bike1.setup_gear(52, 11)
-bike1.setup_wheels(rear_wheel=Wheel(27, 0.7)) # лише заднє колесо
-# bike1.setup_wheels(Wheel(27, 0.5), Wheel(27, 0.7)) # два колеса
+
+bike1 = Bicycle(
+    front_wheel=Wheel(27, 0.5),
+    rear_wheel=Wheel(27, 0.7),
+    gear=Gear(52, 11)
+)
 
 print(
     bike1.gear_inches()
